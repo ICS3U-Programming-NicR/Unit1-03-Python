e46210f891a1fe79e9f4c40ed2e989261724d7ba
Ubuntu <ubuntu@ip-172-31-52-96.ec2.internal>
2022-02-17 14:43:27 +0000

    created code and finished it

---
+++ hello_world_style.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Created By: Nicolas Riscalas
+
+# Date: February 17 2022
+
+# Hello world with proper style
+
+
+def main():
+    print("Hello, World")
+
+
+if __name__ == "__main__":
+    main()
